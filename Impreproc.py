1ef503c31f0cd4242ea52b8ecffb06fd095a1021
molierenguile-makao <moliere.nguile@gmail.com>
2021-03-06 02:38:43 -0500

    Create Impreproc.py

---
+++ Impreproc.py
@@ -0,0 +1,18 @@
+# function that rename the file
+def changeName(path, lastname, newName):
+    """
+
+    Parameters
+    ----------
+    path : string
+        path of the file.
+    lastname : string
+        the current name of the file.
+    newName : string
+        the new name.
+
+    Returns
+    -------
+    the new name
+
+    """
